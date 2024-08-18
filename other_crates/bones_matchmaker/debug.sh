24dd139ee8a835273cf8fb165241a45eeb5aa3cb
Max Whitehead <max.blackllama.alt3@gmail.com>
2024-08-18 13:26:29 -0700

    tokio-console

---
+++ other_crates/bones_matchmaker/debug.sh
@@ -0,0 +1,1 @@
+RUST_LOG=bones_framework=DEBUG,iroh_net=DEBUG,bones_matchmaker=DEBUG,iroh_quinn_udp=DEBUG TOKIO_CONSOLE_BIND=localhost:6667 cargo run &> log.txt
--- other_crates/bones_matchmaker/src/cli.rs
+++ other_crates/bones_matchmaker/src/cli.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use tracing::metadata::LevelFilter;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer};
 
 pub async fn start() {
     configure_logging();
@@ -11,14 +12,29 @@ pub async fn start() {
 }
 
 fn configure_logging() {
-    tracing::subscriber::set_global_default(
-        tracing_subscriber::FmtSubscriber::builder()
-            .with_env_filter(
-                tracing_subscriber::EnvFilter::builder()
-                    .with_default_directive(LevelFilter::INFO.into())
-                    .from_env_lossy(),
-            )
-            .finish(),
-    )
-    .unwrap();
+    let console_layer = console_subscriber::spawn();
+    tracing_subscriber::registry()
+        .with(console_layer)
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_ansi(false)
+                .with_filter(
+                    tracing_subscriber::EnvFilter::builder()
+                        .with_default_directive(LevelFilter::INFO.into())
+                        .from_env_lossy(),
+                ),
+        )
+        .init();
+    // tracing::subscriber::set_global_default(
+    //     tracing_subscriber::FmtSubscriber::builder()
+    //         .with_env_filter(
+    //             tracing_subscriber::EnvFilter::builder()
+    //                 .with_default_directive(LevelFilter::INFO.into())
+    //                 .from_env_lossy(),
+    //         )
+    //         .finish(),
+    // )
+    // .unwrap();
+
+    // console_subscriber::init();
 }
